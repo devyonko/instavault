9cd8ecf7ae61ebf54e9ce33db710af4f606e30c1
Tejas <debuggertej@gmail.com>
2026-01-20 13:39:53 +0530

    Refactor: Integrate Public OpenGraph Resolver and clean up dependencies

--- scripts/inspect_library.js
+++
@@ -1,5 +0,0 @@
-const lib = require('instagram-url-direct');
-console.log('Type:', typeof lib);
-console.log('Value:', lib);
-console.log('Keys:', Object.keys(lib));
-console.log('DEFAULT export:', lib.default);
